0fbbe3d71c5a059a99bd2a33942423d8623c666c
Ryzeii <antypovanton93@gmail.com>
2024-11-14 17:28:54 +0500

    Add files via upload

---
+++ src/component/TextInput.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface TextInputProps {
+  placeholder: string;
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const TextInput: React.FC<TextInputProps> = ({
+  placeholder,
+  value,
+  onChange,
+}) => {
+  return (
+    <input
+      type="text"
+      placeholder={placeholder}
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
+};
+
+export default TextInput;
